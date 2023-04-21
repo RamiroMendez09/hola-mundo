84858bf2db87c724c25d7c052550a94cfd623226
RamiroMendez09 <131395919+RamiroMendez09@users.noreply.github.com>
2023-04-21 13:18:40 -0300

    Create claseN1.php

---
+++ claseN1.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>IES Juan Mantovani</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+  <h1>Bienvenidos: clase N° 1</h1>
+  <h2>Practicas Profesionalizantes clas</2>
+</body>
+</html>
